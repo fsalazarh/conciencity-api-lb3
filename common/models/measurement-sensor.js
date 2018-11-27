0ecc4338570fb4d44741d275cea62488e28518ac
Francisco Salazar <francisco.salazar.12@sansano.usm.cl>
2018-11-26 23:53:45 -0300

    add remote hook for sensor measurements

--- common/models/measurement-sensor.js
+++ common/models/measurement-sensor.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(Measurementsensor) {
-
+    var Model = Measurementsensor
+    Model.beforeRemote('create', function(context, next) {
+        context.args.data.sensorId = "5bfc1032c552a65208aedd82"
+        next()
+      });
 };
