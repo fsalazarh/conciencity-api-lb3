72b835aa7e2656ac853d825757461488e490931f
Francisco Salazar <francisco.salazar.12@sansano.usm.cl>
2018-11-27 16:39:40 -0300

    Update measurement-sensor.js

--- common/models/measurement-sensor.js
+++ common/models/measurement-sensor.js
@@ -2,8 +2,8 @@
 
 module.exports = function(Measurementsensor) {
     var Model = Measurementsensor
-    Model.beforeRemote('create', function(context, next) {
+    Model.beforeRemote('create', function(context, modelInstance, next) {
         context.args.data.sensorId = "5bfc1032c552a65208aedd82"
-        next()
+        next();
     });
 };
