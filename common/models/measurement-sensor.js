fb9337db82b5aeffe123b306ec43e76b0b2bd313
Francisco Salazar <francisco.salazar.12@sansano.usm.cl>
2018-11-27 16:44:51 -0300

    Update measurement-sensor.js

--- common/models/measurement-sensor.js
+++ common/models/measurement-sensor.js
@@ -3,6 +3,7 @@
 module.exports = function(Measurementsensor) {
     var Model = Measurementsensor
     Model.beforeRemote('create', function(context, modelInstance, next) {
+        context.args.data.date = new Date();
         context.args.data.sensorId = "5bfc1032c552a65208aedd82"
         next();
     });
