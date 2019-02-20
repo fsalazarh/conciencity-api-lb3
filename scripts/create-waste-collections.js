3eeb1a23c53e3f2d84bf153afc83f8292f86901a
Francisco Salazar <francisco.salazar.12@sansano.usm.cl>
2019-02-20 12:09:06 -0300

    added script for measurementSensor

--- scripts/create-waste-collections.js
+++ scripts/create-waste-collections.js
@@ -8,37 +8,38 @@ let Community = Server.models.Community
 let WasteCollection = Server.models.WasteCollection
 
 Community
-  .find({
+	.find({
 		include: [{recyclers: 'scale'},{residences:'bucket'}],
 			fields: ['id']
  	})
 	.then(function(communities){
 		let communitiesJson = communities.map(item => {return item.toJSON()})
-		communitiesJson.forEach(function(community){
-		let scaleId = utils.validId(community.recyclers[0].scale['id']) //First recycler
 
-		//Loop for residences of community
-		community.residences.forEach(function(residences){
-    	let bucketId = utils.validId(residences.bucket['id'])
-    	let data = {
-    		scaleId: scaleId,
-    		bucketId: bucketId,
-    		weight: (function(){ return Math.floor((Math.random() * 20) + 1);})()
-    	}
-    	debug(data)
-    	WasteCollection
-    		.create(data)
-    		.then(function(wasteCollection) {
-    			debug(wasteCollection)
-    			return null
-    		})
-    		.catch(function(err) {
-    			debug(err)
-    			return null
-    		})
+		communitiesJson.forEach(function(community){
+			let scaleId = utils.validId(community.recyclers[0].scale['id']) //First recycler
+
+			//Loop for residences of community
+			community.residences.forEach(function(residences){
+				let bucketId = utils.validId(residences.bucket['id'])
+				let data = {
+					scaleId: scaleId,
+					bucketId: bucketId,
+					weight: (function(){ return Math.floor((Math.random() * 20) + 1);})()
+				}
+				debug(data)
+				WasteCollection
+					.create(data)
+					.then(function(wasteCollection) {
+						debug(wasteCollection)
+						return null
+					})
+					.catch(function(err) {
+						debug(err)
+						return null
+					})
+			})
 		})
 	})
-})
-.catch(function(err) {
-	debug(err)
-})
+	.catch(function(err) {
+		debug(err)
+	})		
