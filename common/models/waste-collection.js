24a475dc5adebe6694bf9f612e5670ee79e88eb1
Francisco Salazar <francisco.salazar.12@sansano.usm.cl>
2019-03-03 10:05:42 -0300

    after save wasteCollection hook
    Update the countCollection attribute for the residence

--- common/models/waste-collection.js
+++ common/models/waste-collection.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Wastecollection) {
-
+  var Model = Wastecollection
+  Model.observe('after save', function updateCollectionCount(ctx, next){
+    if(ctx.instance){
+      Model.findOne({
+        where: {
+          bucketId : ctx.instance.bucketId
+        },
+        include: {bucket: 'residence'}
+      })
+      .then(function(res){
+        let response = res.toJSON()
+        let countCollection = response.bucket.residence.countCollection
+        Model.app.models['Residence'].updateAll(
+          {id : response.bucket.residenceId},
+          {countCollection: countCollection+1},
+          function(err, info){
+            if(err) return err
+            return info
+          }
+        )
+      }      
+    )}
+  })
 };
