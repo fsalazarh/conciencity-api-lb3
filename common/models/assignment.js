8236b8f2215a53fce8994ba7e283f795c8764dc0
Francisco Salazar <francisco.salazar.12@sansano.usm.cl>
2019-11-15 08:23:02 -0300

    Update project

--- common/models/assignment.js
+++ common/models/assignment.js
@@ -1,5 +1,17 @@
 'use strict';
+var debug = require('debug')('loopback:log:models:assignment');
+
 
 module.exports = function(Assignment) {
-
+  const Model = Assignment
+  Model.beforeRemote('patchAttributes', function(context, modelInstance, next){
+    console.log("Before remote")
+    if(context.args.data.user_type == "Recycler"){
+      Model.app.models['Recycler'].findById(context.args.data.user_id)
+      .then(function(response){
+        response = response.toJSON()
+        debug(response);
+      })
+    }
+  });
 };
