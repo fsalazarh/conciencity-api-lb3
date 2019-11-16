a044691de458bd170fdd207a593a4069c5609cd8
Francisco Salazar <francisco.salazar.12@sansano.usm.cl>
2019-11-16 10:36:24 -0300

    Add resolution of Assignment: Embed Scale to Recycler

--- common/models/assignment.js
+++ common/models/assignment.js
@@ -4,14 +4,58 @@ var debug = require('debug')('loopback:log:models:assignment');
 
 module.exports = function(Assignment) {
   const Model = Assignment
-  Model.beforeRemote('patchAttributes', function(context, modelInstance, next){
-    console.log("Before remote")
-    if(context.args.data.user_type == "Recycler"){
-      Model.app.models['Recycler'].findById(context.args.data.user_id)
-      .then(function(response){
-        response = response.toJSON()
-        debug(response);
-      })
-    }
-  });
+  Model.beforeRemote('prototype.patchAttributes', function(context, modelInstance, next){
+    // Get Instance from Assignment collection
+    let instance = Model.findById(context.req.params.id)
+    .then(function(assignment){   
+      //Check if instance is a Resident or Recycler
+      debug(assignment)
+      if(assignment.user_type == "Resident"){
+        //Check Buckets availables
+        Model.app.models['Bucket'].findOne({
+          where: {
+            active: false
+          }
+        })
+        .then(function(bucket){
+          bucket = bucket.toJSON();
+
+          //TODO: Validar que el residente no tenga buckets asociados 
+
+          Model.app.models['Resident'].updateAll({
+            id: assignment.user_id,
+            bucket: bucket
+          })
+        })
+      }
+      else{
+        //findOne non assigned scale
+        Model.app.models['Scale'].findOne({
+          where: {
+            active: false
+          }
+        })
+        .then(async (scale) => {
+          //scale = scale.toJSON();
+
+          //TODO: Validar que el reciclador no tenga scales asociados 
+
+          //Update scale instance
+          let updateScale = scale.updateAttributes({
+            active: true, 
+            assigned_at: new Date().toLocaleString(),
+            recyclerId: assignment.user_id
+          })
+          const updatedScale = await updateScale;
+          debug(updatedScale);
+
+          //Assign Scale to Recycler
+          let recycler = await Model.app.models['Recycler'].findById(assignment.user_id);
+          const updateAttr = recycler.updateAttributes({scales: [scale]});
+          const updatedRecycler = await updateAttr;
+          debug(updatedRecycler);
+        })
+      }
+    })
+  })
 };
