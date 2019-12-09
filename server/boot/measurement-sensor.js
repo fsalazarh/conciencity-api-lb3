6994fce3fb475fb2f248a46fef1a04b0b3181fbd
Francisco Salazar <francisco.salazar.12@sansano.usm.cl>
2019-12-09 16:28:10 -0300

    update resident model

--- server/boot/measurement-sensor.js
+++ server/boot/measurement-sensor.js
@@ -1,25 +1,25 @@
 var utils = require('../../lib/utils');
 
 module.exports = function(app) {
-    setInterval(function(){
-        app.models.Community.find({
-            include: {composter: {slot: 'sensor'}}
-        })
-        .then(function(res){
-            let response = res.map(item => {return item.toJSON()})
-            //console.log('sensors: ', response)
-            response.forEach(function(response){
-                let sensorId = response.composter.slot.sensor['id']
-                //POST measurementSensor for each sensor
-                app.models.MeasurementSensor.create({
-                    parameter: "humidity",
-                    value: utils.getRandomArbitrary(0.6, 0.9),
-                    sensorId: sensorId
-                })
-            })
-        })
-        .catch(function(err){
-            console.log(err)
-        })
-    }, 3600000);
+    // setInterval(function(){
+    //     app.models.Community.find({
+    //         include: {composter: {slot: 'sensor'}}
+    //     })
+    //     .then(function(res){
+    //         let response = res.map(item => {return item.toJSON()})
+    //         //console.log('sensors: ', response)
+    //         response.forEach(function(response){
+    //             let sensorId = response.composter.slot.sensor['id']
+    //             //POST measurementSensor for each sensor
+    //             app.models.MeasurementSensor.create({
+    //                 parameter: "humidity",
+    //                 value: utils.getRandomArbitrary(0.6, 0.9),
+    //                 sensorId: sensorId
+    //             })
+    //         })
+    //     })
+    //     .catch(function(err){
+    //         console.log(err)
+    //     })
+    // }, 3600000);
 };
